092cbc686f40ef57603e520bfda1eef2dc248cd8
Magno Manjaro <magnocruz@protonmail.com>
2022-02-05 16:21:35 -0300

    Mal e porcamente resolvido.

--- javascript.js
+++ javascript.js
@@ -1,33 +1,73 @@
 function computerPlay () {
 
-	play = Math.floor(Math.random() * 3);
-	if (play == 0) {
-		return 'rock';
-	}
-	else if (play == 1) {
-		return 'paper';
-	}
-	else {return 'scissors'};
+		play = Math.floor(Math.random() * 3);
+		if (play == 0) {
+			return 'pedra';
+		}
+		else if (play == 1) {
+			return 'papel';
+		}
+		else {return 'tesoura'};
 
 
-}
+	}
 
 function playRound (playerSelection, computerSelection) {
-	if (playerSelection == computerSelection) {
-		return 'Empate';
-	}
-
-	if (playerSelection == 'rock') {
-		if (computerPlay == 'paper') {
-			return 'Você perdeu!';
+		if (playerSelection == computerSelection) {
+			return 'Empate';
 		}
-		else {return 'Você ganhou!'};
+
+		if (playerSelection === 'pedra') {
+			if (computerSelection === 'tesoura') {
+				return 'Você ganhou!';
+			}
+			else {return 'Você perdeu!'};
+		}
+
+		if (playerSelection === 'papel') {
+			if (computerSelection === 'pedra') {
+				return 'Você ganhou!';
+			}
+			else {return 'Você perdeu!'};
+		}
+
+		if (playerSelection === 'tesoura') {
+			if (computerSelection === 'pedra') {
+				return 'Você perdeu!';
+			}
+			else {return 'Você ganhou!'};
+		}
 	}
 
-	return 'Não funcionou';
+
+
+
+
+
+
+let vitoria = 0, derrota = 0, empate = 0;
+
+for (i = 0; i < 5; i++) {
+
+	let playerSelection = prompt('Escolha entre pedra, papel e tesoura, e digite abaixo');
+
+
+	const computerSelection = computerPlay();
+	console.log(`A máquina escolheu ${computerSelection}`);
+	console.log(playRound(playerSelection, computerSelection));
+
+	console.log(i);
+
+
+	if (playRound(playerSelection, computerSelection)=='Você ganhou!') {
+		vitoria++;
+	} else if (playRound(playerSelection, computerSelection)=='Você perdeu!') {
+		derrota++;
+
+	} else {
+		empate++;
+	}
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(`O que a máquina escolher foi ${computerSelection}`);
-console.log(playRound(playerSelection, computerSelection));
+
+console.log(`De 5 partidas, você ganhou ${vitoria}, perdeu ${ derrota}, e empatou ${empate}.`);
